96a2d6caadc280937be4f264783635d521be9812
Jorge Moratilla <jorge@moratilla.com>
2025-01-08 17:47:33 +0100

    feat: add some milestones

--- simple-clicker/src/scenes/Game.ts
+++ simple-clicker/src/scenes/Game.ts
@@ -11,22 +11,32 @@ export class Game extends Scene
     button: Phaser.GameObjects.Text;
     textScore: Phaser.GameObjects.Text;
     clicked: boolean = false;
+    shader: Phaser.GameObjects.Shader;
 
     preload ()
     {
         this.load.setPath('assets');
         
         this.load.image('background', 'bg.png');
-        //this.load.image('logo', 'logo.png');
         this.load.audio('click', 'sounds/click.wav');
+
+        this.load.image('metal', 'textures/alien-metal.jpg');
+        this.load.image('grass', 'textures/grass.png');
+        this.load.image('tiles', 'textures/tiles.jpg');
+        this.load.glsl('bundle', 'shaders/bundle.glsl.js');
+        this.load.audio('music', 'sounds/audio.mp3');
     }
 
     create ()
     {
         
-        this.add.image(512, 384, 'background');
         const textStyle: object = { fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff', stroke: '#000000', strokeThickness: 8 };
+        this.add.image(512, 384, 'background');
+
+        this.shader = this.add.shader('Tunnel', 512, 384, 512, 512, [ 'tiles', 'green', 'metal' ]).setVisible(false);
         
+
+
         this.button = this.add.text(512, 384, 'Click me', textStyle).setInteractive().on('pointerdown', () => {
             // this.button.setStyle({ color: '#a0a0a0' });
             this.score++;
@@ -34,27 +44,40 @@ export class Game extends Scene
             this.clicked = true;
         }).setOrigin(0.5);
         
-        
-        // this.add.tween({
-        //     targets: this.button,
-        //     scale: 1.05,
-        //     duration: 800,
-        //     alpha: 0.1,
-        //     yoyo: true,
-        //     repeat: -1,
-        // });
 
         this.textScore = this.add.text(512, 450, 'Score: ' + this.score, textStyle).setOrigin(0.5);
         
+
     }
 
     update() {
         if (this.clicked)
-            {
-                this.buttonEffect();
-                this.textScore.setText('Score: ' + this.score);
-                this.clicked = false;
-            } 
+        {
+            this.buttonEffect();
+            this.textScore.setText('Score: ' + this.score);
+            this.clicked = false;
+
+            // Milestones
+            switch(this.score) {
+                case 5:
+                {
+                    this.sound.play('music');
+                    break;
+                }
+                case 15: 
+                {
+                    this.shader.setVisible(true);
+                    break;
+                }
+                case 30:
+                {
+                    var textureKey = this.shader.getUniform('iChannel0').textureKey;
+                    console.log(textureKey);
+                    this.shader.setChannel0('green');
+                    break;
+                }
+            }
+        }
 
     }
 
