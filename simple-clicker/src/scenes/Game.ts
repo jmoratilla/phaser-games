c037c10895e8b36c135e008a9021c600e9bb4eb1
Jorge Moratilla <jorge@moratilla.com>
2025-01-14 00:02:16 +0100

    wip: enable menu options

--- simple-clicker/src/scenes/Game.ts
+++ simple-clicker/src/scenes/Game.ts
@@ -2,10 +2,15 @@ import { Scene } from 'phaser';
 
 export class Game extends Scene
 {
+    options: { id: number, text: string, y: number, cost: number, action: () => void }[];
+
     constructor ()
     {
         super('Game');
+
     }
+
+    score: number = 0;
 
     preload ()
     {
@@ -23,6 +28,15 @@ export class Game extends Scene
 
     create ()
     {
+        const options: { name: number, text: string, y: number, cost: number, action: () => void }[] = [
+            { name: 0, text: 'Option 1.....................5 clicks', y: 50, cost: 5, action: () => {this.option(option)} },
+            { name: 1, text: 'Option 2...................10 clicks', y: 100, cost: 10, action: () => {this.option(option)} },
+            { name: 2, text: 'Option 3...................25 clicks', y: 150, cost: 25, action: () => {this.option(option)} },
+            { name: 3, text: 'Option 4...................50 clicks', y: 200, cost: 50, action: () => {this.option(option)} },
+            { name: 4, text: 'Option 5.................100 clicks', y: 250, cost: 100, action: () => {this.option(option)} },
+            { name: 5, text: 'Option 6...............1000 clicks', y: 300, cost: 1000, action: () => {this.option(option)} },
+            { name: 6, text: 'Option 7.............10000 clicks', y: 350, cost: 10000, action: () => {this.option(option)} }
+        ];
         
         const textStyle: object = { fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff', stroke: '#000000', strokeThickness: 8 };
         const background: Phaser.GameObjects.Image = this.add.image(512, 384, 'background');
@@ -41,45 +55,34 @@ export class Game extends Scene
         button.on('pointerdown', () => {
             this.buttonEffect(button);
             button.data.set('score', button.data.get('score') + 1);
+            this.score = button.data.get('score');
             this.sound.play('click');
-            textScore.setText('Score: ' + button.data.get('score'));
+            // textScore.setText('Score: ' + button.data.get('score'));
+            textScore.setText('Score: ' + this.score);
         })
 
         // Marketplace
-        const container1: Phaser.GameObjects.Container = this.add.container(0, 0);
+        const container: Phaser.GameObjects.Container = this.add.container(0, 0);
         const bg1: Phaser.GameObjects.Graphics = this.add.graphics({ fillStyle: { color: 0xffffff } });
         bg1.fillRect(0, 0, 400, 400);
-        container1.add(bg1);
+        container.add(bg1);
         
         const closeButton: Phaser.GameObjects.Text = this.add.text(400-17, 17, '_', { fontFamily: 'Arial Black', fontSize: 24, color: '#ffffff', stroke: '#000000', strokeThickness: 8 }).setOrigin(0.5).setInteractive();
         closeButton.setDataEnabled();
         closeButton.data.set('state', 'open');
-        container1.add(closeButton);
-        container1.setPosition((Number(this.game.config.width)-Number(200))/2, Number(this.game.config.height)/2);
-        
-        const options = [
-            { id: 1, text: 'Option 1.....................5 clicks', y: 50, action: () => {this.option(1)} },
-            { id: 2, text: 'Option 2...................10 clicks', y: 100, action: () => {this.option(2)} },
-            { id: 3, text: 'Option 3...................25 clicks', y: 150, action: () => {this.option(3)} },
-            { id: 4, text: 'Option 4...................50 clicks', y: 200, action: () => {this.option(4)} },
-            { id: 5, text: 'Option 5.................100 clicks', y: 250, action: () => {this.option(5)} },
-            { id: 6, text: 'Option 6...............1000 clicks', y: 300, action: () => {this.option(6)} },
-            { id: 7, text: 'Option 7.............10000 clicks', y: 350, action: () => {this.option(7)} }
-        ];
-        
+        container.add(closeButton);
+        container.setPosition((Number(this.game.config.width)-Number(200))/2, (Number(this.game.config.height)+Number(700))/2);
+
+        // Options
         options.forEach(option => {
-            const text: Phaser.GameObjects.Text = this.add.text(10, option.y, option.text, { fontFamily: 'Arial Black', fontSize: 24, color: '#000000', stroke: '#ffffff', strokeThickness: 8 }).setOrigin(0);
-            container1.add(text);
-            text.setInteractive();
-            text.on('pointerdown', () => {
-                option.action();
-            });
+            const text: Phaser.GameObjects.Text = this.add.text(10, option.y, option.text, { fontFamily: 'Arial Black', fontSize: 24, color: '#cacaca', stroke: '#ffffff', strokeThickness: 8 }).setOrigin(0);
+            container.add(text);
         });
 
         closeButton.on('pointerdown', () => {
             if (closeButton.data.get('state') === 'close') {
                 this.tweens.add({
-                    targets: container1,
+                    targets: container,
                     y: Number(this.game.config.height)/2,
                     duration: 500,
                     ease: 'Linear'
@@ -87,7 +90,7 @@ export class Game extends Scene
                 closeButton.data.set('state', 'open');
             } else {
                 this.tweens.add({
-                    targets: container1,
+                    targets: container,
                     y: 735,
                     duration: 500,
                     ease: 'Linear'
@@ -102,6 +105,10 @@ export class Game extends Scene
             switch(value) {
                 case 5:
                 {
+                    let option = container.getByName(value);
+                    // TODO: Enable option
+                    // option.text.setColor('#000000');
+                    // option.setInteractive(true);
                     this.sound.play('music');
                     break;
                 }
@@ -148,8 +155,18 @@ export class Game extends Scene
         }
     }
 
-    option (id: number): void
+    enableOption(this: Phaser.Game, id: number): void
     {
-        console.log('Option ' + id + ' clicked');
+        let option = this.options[id];
+        console.log('option: ' + option);
+        option.text.setColor('#000000');
+        option.setInteractive(true);
+        console.log('Option ' + id + ' enabled');
+    }
+
+    option (option: { id: number, text: string, y: number, cost: number, action: () => void }): void
+    {
+        console.log('Option ' + option.id + ' clicked');
+        this.score -= option.cost;
     }
 }
